f12184a21d7f0a8d7fe261074a96d7f93f5a3434
Surna srikanth <122173096+srikanthsurna@users.noreply.github.com>
2024-11-01 22:55:19 +0530

    Delete server directory

--- server/connect.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
-async function connectTOMongoDB(url) {
-  return mongoose.connect(process.env.CONNECTIONSTRING);
-}
-module.exports = { connectTOMongoDB };
--- server/routes/auth.route.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const { signup,signin,signout } = require('../controllers/auth.controller');
-const router=express.Router()
-
-router.post("/signup",signup);
-router.post("/signin",signin);
-router.get("/signout",signout);
-
-module.exports=router;
